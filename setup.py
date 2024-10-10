95c026788f2efb1b05360103f6139aad2549ef18
DESKTOP-5TJPE1I\Jose <japeralsolerllc@gmail.com>
2024-10-10 10:34:25 +0200

    solax_x1_boost

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="solax",
+    name="solax_x1_boost",
     use_scm_version=True,
     author="Robin Wohlers-Reichel",
     author_email="me@robinwr.com",
-    description="Solax inverter API client",
+    description="solax_x1_boost inverter API client",
     long_description=long_description,
     long_description_content_type="text/markdown",
     license="MIT",
