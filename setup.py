6561820edf37bcbe0e52c4ea12a37e5d374fac95
MSI\Jose <japeralsoler@gmail.com>
2025-08-04 16:57:38 +0200

    fixed problems

--- setup.py
+++ setup.py
@@ -1,28 +1,26 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="solax",
-    use_scm_version=True,
+    use_scm_version=True,  # Use setuptools_scm for automatic version management
     author="Robin Wohlers-Reichel",
     author_email="me@robinwr.com",
-    description="solax inverter API client",
+    description="Solax inverter API client",
     long_description=long_description,
     long_description_content_type="text/markdown",
     license="MIT",
     url="https://github.com/japeral/solax",
     packages=setuptools.find_packages(exclude=["tests", "tests.*"]),
     install_requires=[
-        "aiohttp>=3.5.4, <4",
+        "aiohttp>=3.5.4,<4",
         "voluptuous>=0.11.5",
         "importlib_metadata>=3.6; python_version<'3.10'",
         "typing_extensions>=4.1.0; python_version<'3.11'",
     ],
-    setup_requires=[
-        "setuptools_scm",
-    ],
+    setup_requires=["setuptools_scm"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -35,3 +33,4 @@ setuptools.setup(
         ],
     },
 )
+
